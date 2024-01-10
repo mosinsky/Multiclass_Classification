c33ab1236c1499cfb260e637b82215338ef53161
mosinsky <mosmich2001@gmail.com>
2024-01-10 19:11:13 +0100

    gradio

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import spacy
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from spacy.lang.en.stop_words import STOP_WORDS
 from nltk.tokenize import word_tokenize
-from googletrans import Translator
-# from translate import Translator
 
 # Load the pre-trained model and preprocessing components
 model = joblib.load('pre-trained_models/logistic_regression_model.joblib')
@@ -15,6 +13,7 @@ tfidf_t = joblib.load('pre-trained_models/tfidf_transformer_model.joblib')
 
 # Load spaCy for lemmatization
 nlp = spacy.load("en_core_web_sm")
+
 
 # Define your text cleaning function
 def clean_text(text):
@@ -60,44 +59,38 @@ def remove_pos_tags(text):
     sent = [token.text for token in doc if token.tag_ == 'NN']
     return ' '.join(sent)
 
-def translate_to_english(text):
-    # Translate text to English using googletrans
-    translator = Translator()
-    translated_text = translator.translate(text, src='auto', dest='en').text
-    return translated_text
 
 
 # Combine preprocessing and classification
 def classify_text(input_text):
     if not input_text:
         return "", "", "Input text is empty. Please enter some text."
-
-    cleaned_text = clean_text(input_text)
-    preprocessed_text = preprocess_text(cleaned_text)  # Use the preprocessing steps you need
+    preprocessed_text = preprocess_text(input_text)  # Use the preprocessing steps you need
     lemmatized_text = lemmatize(preprocessed_text)  # You can choose to include or exclude lemmatization
-    text_without_pos_tags = remove_pos_tags(lemmatized_text)  # You can choose to include or exclude removing POS tags
+    # text_without_pos_tags = remove_pos_tags(lemmatized_text)  # You can choose to include or exclude removing POS tags
+
 
     # Vectorize and transform the cleaned text
-    test = cv.transform([text_without_pos_tags])  # Use the processed text
+    test = cv.transform([preprocessed_text])  # Use the processed text
     test_tfidf = tfidf_t.transform(test)
 
     # Make prediction using the loaded model
     prediction = model.predict(test_tfidf)[0]
 
-    return text_without_pos_tags, prediction
+    return preprocessed_text, prediction
 
 # Create a Gradio interface with an API endpoint
 iface = gr.Interface(
-    # fn=classify_text,
-    fn=lambda x: (translate_to_english(x),) + classify_text(x),
-    inputs=gr.Textbox(placeholder="Enter text...", label = "Complaint"),
-
-    outputs=[gr.Textbox(placeholder="Translated Text", label = "Preprocessed_text"),
-              gr.Textbox(placeholder="leaned Text", label = "Prediction"),
-              gr.Textbox(placeholder="Prediction", label = "Prediction")]
+    fn=classify_text,
+    inputs=gr.Textbox(placeholder="Enter text...", label="Complaint"),
+    outputs=[
+        gr.Textbox(placeholder="text_without_pos_tags", label="cleaned text"),
+        gr.Textbox(placeholder="Prediction", label="Prediction")]
 )
-iface.launch(share=True)
 
-# I tried to make a transaction at a supermarket retail store, using my chase \
-# debit/atm card, but the transaction was declined. I am still able to withdraw money out of an \
-# ATM machine using the same debit card. Please resolve this issue.
+# Launch the Gradio interface
+iface.launch()
+
+# I tried to make a transaction at a supermarket retail store, using my chase debit/atm card, but the transaction was declined. I am still able to withdraw money out of an ATM machine using the same debit card. Please resolve this issue.
+            #  gr.Textbox(placeholder="Translated Text", label = "Preprocessed_text"),
+
