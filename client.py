aea07c99a78c352be152f0eb09683208fef04f6d
mosinsky <mosmich2001@gmail.com>
2024-02-04 00:28:25 +0100

    new.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from gradio_client import Client
 input = input("Complaint Text: ")
 client = Client("http://127.0.0.1:7860/")
 result = client.predict(
-		input,	# str  in 'Complaint' Textbox component
+		input,
 		api_name="/predict"
 )
 print(result)
