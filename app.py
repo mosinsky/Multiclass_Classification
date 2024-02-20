8d3d23b6ada20ebe286db0286d770382ba253a19
mosinsky <mosmich2001@gmail.com>
2024-02-21 00:20:29 +0100

    final_app

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import joblib
 import re
 import spacy
 import nltk
-# nltk.download()
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from spacy.lang.en.stop_words import STOP_WORDS
 from nltk.tokenize import word_tokenize
@@ -12,25 +11,22 @@ import warnings
 warnings.filterwarnings('ignore')
 from autocorrect import Speller
 spell = Speller(lang='en')
+nlp = spacy.load("en_core_web_sm")
 
-# Load the pre-trained model and preprocessing components
+# Load the pre-trained models
 model = joblib.load('pre-trained_models/logistic_regression.joblib')
 cv = joblib.load('pre-trained_models/cv.joblib')
 tfidf_t = joblib.load('pre-trained_models/tfidf.joblib') 
 
-# Load spaCy for lemmatization
-nlp = spacy.load("en_core_web_sm")
 
 def correct_text(text):
-    text = spell(text)
+    spell = Speller(lang='en')
+    corrected_text = spell(text)
+    
+    return corrected_text
 
-    return text
 
 def clean_text(text):
-    if text is None:
-        return ""
-    
-    # Remove special characters, numbers, and punctuation
     text = re.sub(r'[^a-zA-Z\s]', '', text)
     text = re.sub('\S*\d\S*\s*','', text)
     text = re.sub('\[.*\]','', text)
@@ -43,6 +39,7 @@ def remove_stopwords(tokens):
     return filtered_tokens
 
 def tokenize_text(text):
+    # Tokenize the text into words
     tokens = word_tokenize(text)
 
     return tokens
@@ -50,27 +47,29 @@ def tokenize_text(text):
 
 def preprocess_text(text):
     cleaned_text = clean_text(text)
-    tokens = tokenize_text(cleaned_text) 
-    tokens_without_stopwords = remove_stopwords(tokens) 
+    tokens = tokenize_text(cleaned_text)
+    tokens_without_stopwords = remove_stopwords(tokens)
     preprocessed_text = ' '.join(tokens_without_stopwords)
+    
     return preprocessed_text
 
 def lemmatize(text):
     doc = nlp(text)
     sent = [token.lemma_ for token in doc if token.text not in STOP_WORDS]
+
     return ' '.join(sent)
 
 def remove_pos_tags(text):
     doc = nlp(text)
     sent = [token.text for token in doc if token.tag_ == 'NN']
-    return ' '.join(sent)
 
+    return ' '.join(sent)
 
 
 # Combine preprocessing and classification
 def classify_text(input_text):
-    # if not input_text:
-    #     return "", "", "Input text is empty. Please enter some text."
+    if not input_text:
+        return "", "", "Please describe Your issue first."
     input_text = input_text[0] if isinstance(input_text, list) else input_text
     input_text = correct_text(input_text)
     preprocessed_text = preprocess_text(input_text)
